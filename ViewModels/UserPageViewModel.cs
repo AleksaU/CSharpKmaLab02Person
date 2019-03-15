296fc643757b44a9d010ca795a9db911c24548aa
AleksaU <sangen2901@gmail.com>
2019-03-15 21:43:08 +0200

    UserPageViewModel

--- ViewModels/UserPageViewModel.cs
+++ ViewModels/UserPageViewModel.cs
@@ -1,12 +1,119 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Lab02Utkina.Models;
+using Lab02Utkina.Tools;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
 
 namespace Lab02Utkina.ViewModels
 {
-    class UserPageViewModel
+    internal class UserPageViewModel : INotifyPropertyChanged
     {
+
+        private string _name;
+        private string _surName;
+        private string _email;
+        private DateTime _birthDate;
+
+        private readonly bool _isAdult;
+        private readonly string _sunSign;
+        private readonly string _chineseSign;
+        private readonly bool _isBirthday;
+
+       
+        private RelayCommand<object> _proceedCommand;
+
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            private set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string SurName
+        {
+            get
+            {
+                return _surName;
+            }
+            set
+            {
+                _surName = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                _email = value;
+                OnPropertyChanged();
+            }
+        }
+        public string BirthDate
+        {
+            get
+            {
+                return ("birth " + _birthDate);
+            }
+            set
+            {
+                _birthDate = Convert.ToDateTime(value);
+                OnPropertyChanged();
+            }
+        }
+
+
+
+        public RelayCommand<Object> ProceedCommand
+        {
+            get
+            {
+                return _proceedCommand ?? (_proceedCommand = new RelayCommand<object>(
+                           o =>
+                           {
+                               Person person = new Person(_name, _surName, _email, _birthDate);
+                               MessageBox.Show(
+                                                           $"First name: {person.Name}\n" +
+                                                           $"Last name: {person.Surname}\n" +
+                                                           $"Email: {person.Email}\n" +
+                                                           $"Date of birth: {person.BirthDate}\n" +
+                                                           $"Adult: {person.IsAdult}\n" +
+                                                           $"Our Sign: {person.SunSign}\n" +
+                                                           $"Chinese Sign: {person.ChineseSign}\n" +
+                                                           $"{person}"
+                                                       );
+
+                           }, o => CanExecuteCommand()));
+            }
+        }
+
+        private bool CanExecuteCommand()
+        {
+            return !string.IsNullOrWhiteSpace(_name) && !string.IsNullOrWhiteSpace(_surName) && !string.IsNullOrWhiteSpace(_email);
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+
     }
 }
--- Views/UserPage.xaml.cs
+++ Views/UserPage.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using Lab02Utkina.ViewModels;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Lab02Utkina.Views
 {
@@ -23,7 +12,7 @@ namespace Lab02Utkina.Views
         public UserPage()
         {
             InitializeComponent();
-         //   DataContext = new UserPageViewModel();
+            DataContext = new UserPageViewModel();
         }
     }
 }
